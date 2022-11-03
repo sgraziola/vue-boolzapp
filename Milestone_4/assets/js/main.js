d6e7b088dc8c891b3afbe269d192d0a5cba9c9f8
sgraziola <simo.fdl@gmail.com>
2022-11-03 11:22:20 +0100

    Boolzapp: Add milestone4 folder

---
+++ Milestone_4/assets/js/main.js
@@ -0,0 +1,60 @@
+/* 
+- Milestone 4
+    ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+*/
+
+import contacts from "./database.js"
+
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            contacts,
+            activeContact : 0,
+            newMessageSent: {
+                date: new Date(),
+                message: '',
+                status: 'sent'
+            },
+            newMessageReceived: {
+                date: new Date(),
+                message: 'Ok',
+                status: 'received'
+            },
+        }
+    },
+    methods: {
+        activeIndex(index){
+            this.activeContact = index;
+        },
+
+        addSentMsg(){
+            const newMessage = {
+                ...this.newMessageSent
+              }
+            //console.log("ho cliccato su enter");
+            this.contacts[this.activeContact].messages.push(newMessage)
+            //console.log(this.contacts[this.activeContact]);
+            // svuoto l'input dopo che il messaggio é stato inviato e aggiunto a text_message
+            this.newMessageSent.message = ''
+            this.addReceivedMsg();
+        },
+
+        addReceivedMsg(){
+            setTimeout(()=>{
+                this.contacts[this.activeContact].messages.push(this.newMessageReceived)
+            }, 1000);
+        },
+
+        time(i){
+            const time = new Date (this.contacts[i].messages[this.contacts[i].messages.length - 1].date);
+            //console.log(time);
+            const hoursAndMinutes = time.getHours() + ':' + time.getMinutes();
+            return hoursAndMinutes;
+
+        }
+        
+
+    }
+}).mount("#app");
