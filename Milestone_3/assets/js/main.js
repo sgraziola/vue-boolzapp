a7a826a1e098e9c29bb317b74ae050e7faef9ee5
sgraziola <simo.fdl@gmail.com>
2022-11-03 10:29:56 +0100

    Boolzapp: fix milestones texts

--- Milestone_3/assets/js/main.js
+++ Milestone_3/assets/js/main.js
@@ -1,6 +1,8 @@
-/* Milestone 1
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto */
+/* 
+- Milestone 3
+    ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+    ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo 
+*/
 
 import contacts from "./database.js"
 
